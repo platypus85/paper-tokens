4d4c26b53627c0d2bfb40d6686f3c426d76e8b37
Alberto Camillo <info@albertocamillo.com>
2018-07-17 22:54:21 +1000

    Fixed Tokens structure and tents.

--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -20,6 +20,7 @@ class Tokens extends Component {
           : "hidden"}>Loading&#8230;</div>
         <div className="printable" id="printed-tokens">
           {this.createTokensList(this.props)}
+          {this.createTokensTents(this.props)}
         </div>
       </div>
     )
@@ -47,6 +48,39 @@ class Tokens extends Component {
     })
     return tokenList;
   }
+  createTokensTents(props) {
+    let tents = [];
+    var tks = props
+      .tokens
+      .slice();
+    tks.sort((a, b) => SizeEnum.properties[a.size].value - SizeEnum.properties[b.size].value).forEach((token, i) => {
+      if (token.showTent) {
+        tents.push(
+          <div className="tent" key={token.id + i || token.url + "" + i}>
+            <div className="side back">
+              <div className="name">
+                <p>{token.name}</p>
+              </div>
+              <div className="notes">
+                <p>Notes:</p>
+              </div>
+            </div>
+            <div className="side front">
+              <div className="name">
+                <p>{token.name}</p>
+              </div>
+              <div className="image">
+                <img alt={token.name} src={token.url} className="creature"/>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    })
+    return tents;
+  }
 };
+
+
 
 export {Tokens};
